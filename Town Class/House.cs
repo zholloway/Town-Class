9d6e2b0284ac79882f727c040ed4e7e48c05f28e
zholloway <zholloway92@gmail.com>
2017-03-27 15:22:33 -0400

    house and bank classes updated and complete

--- Town Class/House.cs
+++ Town Class/House.cs
@@ -22,39 +22,70 @@ namespace Town_Class
 
         public double Length { get; set; }
 
-        public double SquareFootage { get; } // read only
-
         public int NumberOfFloors { get; set; }
 
         public int NumberOfBathrooms { get; set; }
 
-        public double Volume { get; } // read only
+        public int NumberOfBedrooms { get; set; }
 
-        public string FullAddress { get; } // read only
+        private double _squareFootage;
 
-        public string Address1 { get; set; }
+            public double SquareFootage { get { return _squareFootage; } } // read only
 
-        public string Address2 { get; set; }
+            public void CalculateSquareFootage (double Width, double Length, int NumberOfFloors)
+        {
+            this._squareFootage = Width * Length * NumberOfFloors;
+        }
 
-        public string City { get; set; }
+        private double _volume;
 
-        public string State { get; set; }
+            public double Volume { get { return _volume; } } // read only
 
-        public string Zip { get; set; }
+            public void CalculateVolume(double Height, double Width, double Length)
+            {
+                this._volume = Height * Width * Length;
+            }
+
+        private string _fullAddress;
+
+            public string FullAddress { get { return _fullAddress; } } // read only
+
+            public string Address1 { get; set; }
+
+            public string Address2 { get; set; }
+
+            public string City { get; set; }
+
+            public string State { get; set; }
+
+            public string Zip { get; set; }
+
+            public void AssembleFullAddress (string Address1, string Address2, string City, string State, string Zip)
+            {
+                this._fullAddress = Address1 + " " + Address2 + " " + City + ", " + State + " " + Zip;
+            }
 
         private int _currentOccupants;
 
-        public int CurrentOccupants { get; } // read only
+            public int CurrentOccupants { get { return _currentOccupants; } } // read only
 
-        public int IncrementOccupants()
-        {
-            return this._currentOccupants++;
-        }
+            public int IncrementOccupants()
+            {
+                return this._currentOccupants++;
+            }
 
-        public int DecreaseOccupants()
-        {
-            return this._currentOccupants--;
-        }
+            public int DecreaseOccupants()
+            {
+                return this._currentOccupants--;
+            }
+
+            public void SetNumberOfOccupants (int number)
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    this._currentOccupants++;
+                }
+            }
 
         public House()
         {
--- Town Class/Program.cs
+++ Town Class/Program.cs
@@ -10,6 +10,36 @@ namespace Town_Class
     {
         static void Main(string[] args)
         {
+            var house1 = new House
+            {
+                Name = "Home",
+                NumberOfRooms = 7,
+                ConstructionMaterial = "Wood",
+                Height = 15,
+                Width = 400,
+                Length = 800,
+                NumberOfBathrooms = 2,
+                NumberOfBedrooms = 3,
+                NumberOfFloors = 1,
+                Address1 = "7514 Lake Josephine Lane",
+                Address2 = "",
+                City = "Odessa",
+                State = "Florida",
+                Zip = "33556"
+            };
+
+            house1.CalculateSquareFootage(house1.Width, house1.Length, house1.NumberOfFloors);
+            house1.CalculateVolume(house1.Height, house1.Width, house1.Length);
+            house1.SetNumberOfOccupants(3);
+            house1.AssembleFullAddress(house1.Address1, house1.Address2, house1.City, house1.State, house1.Zip);
+
+            Console.WriteLine($"The name of the {house1.BuildingType} is {house1.Name}.");
+            Console.WriteLine($"The square footage of the house is {house1.SquareFootage}.");
+            Console.WriteLine($"The volume of the house is {house1.Volume}");
+            Console.WriteLine($"There are {house1.CurrentOccupants} people living there.");
+            Console.WriteLine($"The address of the house is: {house1.FullAddress}");
+
+            Console.ReadLine();
         }
     }
 }
