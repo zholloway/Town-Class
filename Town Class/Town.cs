bf6424824847c2c4d1852256c6128ef26196859c
zholloway <zholloway92@gmail.com>
2017-03-28 13:46:21 -0400

    town and townsperson classes added to github

---
+++ Town Class/Town.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Town_Class
+{
+    class Town
+    {
+        public List<House> AllTheHouses { get; set; }
+
+        public List<Bank> AllTheBanks { get; set; }
+
+        public List<School> AllTheSchools { get; set; }
+
+        public List<Townsperson> TourGroup { get; set; }
+
+        public void MoveTourGroupToHouse (string houseName)
+        {
+            Console.WriteLine($"The tour group is starting at {houseName}.");
+            foreach (var house in AllTheHouses)
+            {
+                if (house.Name == houseName)
+                {
+                    house.IncrementOccupants(this.TourGroup);
+                }
+            }
+        }
+
+        public void MoveTourGroupFromHouseToBank(string houseName, string bankName)
+        {
+            Console.WriteLine($"The tour group is moving from {houseName} to {bankName}.");
+            foreach (var house in AllTheHouses)
+            {
+                if (house.Name == houseName)
+                {
+                    house.DecreaseOccupants(this.TourGroup);
+                }
+            }
+
+            foreach (var bank in AllTheBanks)
+            {
+                if (bank.Name == bankName)
+                {
+                    bank.IncrementOccupants(this.TourGroup);
+                }
+            }
+        }
+    }
+}
---
+++ Town Class/Townsperson.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Town_Class
+{
+    class Townsperson
+    {
+        public string Name { get; set; }
+
+        public string HairColor { get; set; }
+
+        public string EyeColor { get; set; }
+
+        public Townsperson(string name, string hairColor, string eyeColor)
+        {
+            this.Name = name;
+            this.HairColor = hairColor;
+            this.EyeColor = eyeColor;
+        }
+    }
+}
